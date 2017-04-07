46acba0c72018cc5317330b51845be940a26d527
tanghaichen <964277403@qq.com>
2017-04-07 22:31:08 +0800

    4.07day 22.31

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/More',function (req,res,next) {
+    res.render('More');
+});
+router.get('/classify',function (req,res,next) {
+    res.render('classify');
+});
+
+module.exports = router;
