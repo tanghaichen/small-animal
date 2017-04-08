ae0a63a19773d323d07793fe2ba0123ef10299c2
tanghaichen <964277403@qq.com>
2017-04-08 17:53:36 +0800

    luyoutong

--- routes/backstage.js
+++ routes/backstage.js
@@ -1,38 +1,50 @@
 var express = require('express'),
-    router = express.Router();
-    // myClient = require('mongodb').MongoClient,
-    // ObjectId = require('mongodb').ObjectId,
-    // server = 'http://127.0.0.1:27017/animal',
-    // arts,
-    // tags;
+    router = express.Router(),
+    myClient = require('mongodb').MongoClient,
+    ObjectId = require('mongodb').ObjectId,
+    server = 'mongodb://localhost:27017/animal',
+    arts, tags;
 
-// myClient.connect(server, function(err, data) {
-    // if (!err) {
-        // arts = data.collection('arts');
-        // tags = data.collection('tags');
-    // }
-// })
+myClient.connect(server, function(err, data) {
+    if (!err) {
+        arts = data.collection('arts');
+        tags = data.collection('tags');
+    }
+})
+
+myClient.connect(server, function(err, db) {
+    if (!err) {
+        arts = db.collection('arts');
+        tags = db.collection('tags');
+    }
+})
 
 router.get('/', function(req, res, next) {
-    res.render('public-aside');
+    res.render('tagsadd');
 });
+
+
 //展示文章列表
 router.get('/artslist', function(req, res, next) {
-    // arts.find({}).toArray(function(err, data) {
-        // if (!err) {
-            res.render('artslist',{arts:data})
-        // }
-    // })
+    arts.find({}).toArray(function(err, data) {
+        if (!err) {
+            res.render('artslist', { arts: data })
+        }
+    })
 })
 
 //增加文章
 router.get('/artsadd', function(req, res, next) {
-    res.render('artsadd')
+    tags.find({}).toArray(function(err, data) {
+        res.render('artsadd', { tags: data })
+    })
 })
 
 //展示标签列表
 router.get('/tagslist', function(req, res, next) {
-    res.render('tagslist')
+    tags.find({}).toArray(function(err, data) {
+        res.render('tagslist', { tags: data })
+    })
 })
 
 //增加标签
