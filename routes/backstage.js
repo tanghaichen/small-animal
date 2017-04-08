7aea21d1e2d900aa6e71d5d86b16d165f071a0e2
tanghaichen <964277403@qq.com>
2017-04-08 16:37:50 +0800

    4.30gx

--- routes/backstage.js
+++ routes/backstage.js
@@ -1,13 +1,28 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    router = express.Router();
+    // myClient = require('mongodb').MongoClient,
+    // ObjectId = require('mongodb').ObjectId,
+    // server = 'http://127.0.0.1:27017/animal',
+    // arts,
+    // tags;
 
+// myClient.connect(server, function(err, data) {
+    // if (!err) {
+        // arts = data.collection('arts');
+        // tags = data.collection('tags');
+    // }
+// })
 
 router.get('/', function(req, res, next) {
     res.render('public-aside');
 });
 //展示文章列表
 router.get('/artslist', function(req, res, next) {
-    res.render('artslist')
+    // arts.find({}).toArray(function(err, data) {
+        // if (!err) {
+            res.render('artslist',{arts:data})
+        // }
+    // })
 })
 
 //增加文章
